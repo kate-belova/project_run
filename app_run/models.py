65674d8735e2b6a1708f29da407978c43d095af1
Kate Belova <thebrightestcry@gmail.com>
2025-09-05 14:58:25 +0300

    api/runs/ created

--- app_run/models.py
+++ app_run/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Run(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True, verbose_name='Комментарий')
+    athlete = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                related_name='athlete',
+                                verbose_name='Атлет')
+
+    class Meta:
+        verbose_name = 'Пробежка'
+        verbose_name_plural = 'Пробежки'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Пробежка {self.athlete.username} от {self.created_at}'
--- app_run/views.py
+++ app_run/views.py
@@ -1,11 +1,15 @@
 from django.conf import settings
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from app_run.models import Run
+from app_run.serializers import RunSerializer
 
 
 @api_view(['GET'])
 def company_details(request):
-    """API endpoint для получения информации о компании."""
+    """API endpoint для получения информации о компании"""
     details = {
         'company_name': settings.COMPANY_NAME,
         'slogan': settings.COMPANY_SLOGAN,
@@ -13,3 +17,9 @@ def company_details(request):
     }
 
     return Response(details)
+
+class RunViewSet(viewsets.ModelViewSet):
+    """API для работы с пробежками"""
+
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
